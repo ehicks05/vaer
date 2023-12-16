451552187ea594d9082531e82c9ac35e3e21a643
Eric Hicks <ehicks05@yahoo.com>
2023-12-16 12:53:10 -0500

    get location

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -6,7 +6,7 @@ import { Home } from '@/app';
 
 function MyApp() {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-sky-950 to-blue-950">
+    <div className="flex flex-col min-h-screen bg-gradient-to-r from-stone-900 to-neutral-950">
       <Header />
       <div className="flex-grow flex flex-col h-full sm:px-4">
         <Routes>
--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -4,56 +4,19 @@ import { useStopwatch } from "@/hooks";
 import { BiStopwatch } from "react-icons/bi";
 import { FONT_SIZES, BUTTON_SIZES } from "@/constants";
 import { MdRestartAlt, MdPlayArrow, MdPause } from "react-icons/md";
+import { useGeolocation } from "@uidotdev/usehooks";
 
 export const Home = () => {
-  const {
-    startedAt,
-    paused,
-    laps,
-    displayTime,
-    togglePause,
-    reset,
-    addLap,
-    formatTime,
-  } = useStopwatch();
+  const state = useGeolocation();
+
+  
 
   return (
     <div className="font-mono flex flex-col flex-grow items-center justify-center gap-4">
       <div className="flex flex-col items-center flex-grow">
-        <div className="p-16"></div>
-        <div className={FONT_SIZES.PRIMARY}>{displayTime}</div>
-        <div className="max-h-96 overflow-auto">
-          <table>
-            <tbody>
-              {laps.map((lap, i) => (
-                <tr className={FONT_SIZES.SECONDARY} key={i}>
-                  <td className="px-4">#{lap.lapNumber}</td>
-                  <td className="px-4">{formatTime(lap.lapTime)}</td>
-                  <td className="px-4">{formatTime(lap.totalTime)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
       </div>
       <div className="flex items-center gap-8">
-        {startedAt && (
-          <Button onClick={reset}>
-            <MdRestartAlt className={BUTTON_SIZES.SECONDARY} />
-          </Button>
-        )}
-        <Button onClick={togglePause}>
-          {paused ? (
-            <MdPlayArrow className={BUTTON_SIZES.PRIMARY} />
-          ) : (
-            <MdPause className={BUTTON_SIZES.PRIMARY} />
-          )}
-        </Button>
-        {startedAt && (
-          <Button onClick={addLap} disabled={paused}>
-            <BiStopwatch className={BUTTON_SIZES.SECONDARY} />
-          </Button>
-        )}
+        <pre>{ JSON.stringify(state, null, 2) }</pre>
       </div>
     </div>
   );
