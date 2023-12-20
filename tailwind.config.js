c20edc93e7c0958724fd44c5088516d0539f230b
Eric Hicks <ehicks05@yahoo.com>
2023-12-20 02:06:11 -0500

    biome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 import { fontFamily } from 'tailwindcss/defaultTheme';
 
-export const content = ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"];
+export const content = ['./src/**/*.{js,jsx,ts,tsx}', './index.html'];
 export const theme = {
-  fontFamily: {
-    'sans': ['Fredoka', ...fontFamily.sans],
-    'mono': fontFamily.mono,
-  },
-  extend: {
-    colors: {
-      "caribbean-green": {
-        50: "#f3fefa",
-        100: "#e8fdf6",
-        200: "#c5fbe8",
-        300: "#a1f8d9",
-        400: "#5bf2bd",
-        500: "#15eda1",
-        600: "#13d591",
-        700: "#10b279",
-        800: "#0d8e61",
-        900: "#0a744f",
-      }
-    },
-  },
+	fontFamily: {
+		sans: ['Fredoka', ...fontFamily.sans],
+		mono: fontFamily.mono,
+	},
+	extend: {
+		colors: {
+			'caribbean-green': {
+				50: '#f3fefa',
+				100: '#e8fdf6',
+				200: '#c5fbe8',
+				300: '#a1f8d9',
+				400: '#5bf2bd',
+				500: '#15eda1',
+				600: '#13d591',
+				700: '#10b279',
+				800: '#0d8e61',
+				900: '#0a744f',
+			},
+		},
+	},
 };
 export const variants = {
-  extend: {},
+	extend: {},
 };
-export const plugins = [];
+export const plugins = [require('tailwind-scrollbar')({ nocompatible: true })];
