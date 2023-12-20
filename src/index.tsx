b4c4873a28d57ef0bff3b490ce60abffcdf859e5
Eric Hicks <ehicks05@yahoo.com>
2023-12-20 18:32:14 -0500

    persist query client cache

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,32 @@
-import "./index.css";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ModalProvider } from "react-modal-hook";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider } from 'react-modal-hook';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
-import App from "./MyApp";
+import App from './MyApp';
 
 const queryClient = new QueryClient();
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
+const persister = createSyncStoragePersister({
+	storage: window.localStorage,
+	key: 'vaer-REACT_QUERY_OFFLINE_CACHE',
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container as Element);
 root.render(
-  <BrowserRouter>
-    <ModalProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </ModalProvider>
-  </BrowserRouter>
+	<BrowserRouter>
+		<ModalProvider>
+			<PersistQueryClientProvider
+				client={queryClient}
+				persistOptions={{ persister }}
+			>
+				<App />
+			</PersistQueryClientProvider>
+		</ModalProvider>
+	</BrowserRouter>,
 );
