1a349fa9857d4284b4cff105c9d3080f68cb9847
Eric Hicks <ehicks05@yahoo.com>
2023-12-26 14:00:21 -0500

    wind direction, sunrise, sunset

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -5,7 +5,8 @@ import DailyForecast from './DailyForecast';
 import { useOpenWeatherMap, useWeatherGov } from '@/hooks';
 import { getWeatherIcon } from './weather_icons';
 import { Card } from '@/components';
-import { HiArrowUp } from 'react-icons/hi';
+import { WiDirectionUp, WiSunrise, WiSunset } from 'react-icons/wi';
+import { format } from 'date-fns';
 
 const CurrentConditions = () => {
 	const { oneCallQuery } = useOpenWeatherMap();
@@ -19,7 +20,7 @@ const CurrentConditions = () => {
 	const {
 		current: { temp, feels_like, weather },
 	} = oneCallQuery.data;
-	const { id, icon } = weather[0];
+	const { id, icon, description } = weather[0];
 
 	const { city, state } =
 		pointQuery.data?.properties.relativeLocation.properties || {};
@@ -37,10 +38,16 @@ const CurrentConditions = () => {
 					Feels like <Temp temp={feels_like} />
 				</pre>
 			</div>
-			<Icon className="inline" size={64} />
+			<Icon className="inline" size={64} title={description} />
 		</div>
 	);
 };
+
+const degreeToDirection = (degree: number) => {
+	const DIRECTIONS = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+	const index = Math.round(degree / 45) % DIRECTIONS.length; 
+	return DIRECTIONS[index];
+}
 
 const Wind = () => {
 	const { oneCallQuery } = useOpenWeatherMap();
@@ -51,21 +58,51 @@ const Wind = () => {
 	}
 
 	const {
-		current: { wind_deg, wind_speed, wind_gust },
+		current: { wind_deg, wind_speed },
 	} = oneCallQuery.data;
 
 	return (
-		<div className="max-w-xs sm:max-w-lg md:max-w-2xl w-full">
-			Wind
-			<Card>
-				<div className="flex flex-col gap-4 w-full p-4">
-					<HiArrowUp style={{ transform: `rotate(${wind_deg}deg)` }} />
-					<div>Degree: {wind_deg}</div>
-					<div>Speed: {wind_speed}</div>
-				</div>
-			</Card>
-		</div>
-	);
+    <div className="max-w-xs sm:max-w-lg md:max-w-2xl w-full">
+      Wind
+      <Card>
+        <div className="flex flex-col gap-4 w-full p-4">
+          <WiDirectionUp size={32} style={{ transform: `rotate(${wind_deg}deg)` }} />
+          <div>
+            {wind_speed} mph {degreeToDirection(wind_deg)}
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+const SunriseSunset = () => {
+  const { oneCallQuery } = useOpenWeatherMap();
+  const { data } = oneCallQuery;
+
+  if (!data) {
+    return <div>loading</div>;
+  }
+
+  const {
+   sunrise, sunset,
+  } = oneCallQuery.data.daily[0];
+
+  return (
+    <div className="max-w-xs sm:max-w-lg md:max-w-2xl w-full">
+      Sunrise(set)
+      <Card>
+        <div className="flex flex-col gap-4 w-full p-4">
+          <WiSunrise size={32}/>
+          <div>{format(sunrise * 1000, "h:mm a")}</div>
+        </div>
+        <div className="flex flex-col gap-4 w-full p-4">
+          <WiSunset size={32}/>
+          <div>{format(sunset * 1000, "h:mm a")}</div>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export const Home = () => {
@@ -75,6 +112,7 @@ export const Home = () => {
 			<HourlyForecast />
 			<DailyForecast />
 			<Wind />
+			<SunriseSunset />
 		</div>
 	);
 };
