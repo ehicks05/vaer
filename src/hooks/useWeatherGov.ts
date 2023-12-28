12ffd75ff28907c73a80574864b815cd55de7178
Eric Hicks <ehicks05@yahoo.com>
2023-12-28 16:09:01 -0500

    cleanup

--- src/hooks/useWeatherGov.ts
+++ src/hooks/useWeatherGov.ts
@@ -1,54 +1,19 @@
-import {
-	useGetPoint,
-	useGetForecast,
-	useGetHourlyForecast,
-	useGetGridpoint,
-	useGetStations,
-	useGetStationLatestObservation,
-} from '@/services/weathergov';
+import { useGetPoint } from '@/services/weathergov';
 import { useCachedGeolocation } from './useCachedGeolocation';
-import { round } from 'lodash';
+import { useActiveLocation } from './useActiveLocation';
 
 export const useWeatherGov = () => {
 	const geolocation = useCachedGeolocation();
-	const { latitude, longitude } = geolocation || {};
+	const [activeLocation] = useActiveLocation();
 
-	const pointQuery = useGetPoint({
-		lat:
-			latitude !== null && latitude !== undefined ? round(latitude, 4) : undefined,
-		long:
-			longitude !== null && longitude !== undefined
-				? round(longitude, 4)
-				: undefined,
-	});
+	const { lat, long } = activeLocation
+		? { lat: activeLocation.lat, long: activeLocation.lng }
+		: { lat: String(geolocation.latitude), long: String(geolocation.longitude) };
 
-	// const forecastQuery = useGetForecast({
-	// 	url: pointQuery.data?.properties.forecast,
-	// });
-
-	// const hourlyForecastQuery = useGetHourlyForecast({
-	// 	url: pointQuery.data?.properties.forecastHourly,
-	// });
-
-	// const gridpointQuery = useGetGridpoint({
-	// 	url: pointQuery.data?.properties.forecastGridData,
-	// });
-
-	// const stationsQuery = useGetStations({
-	// 	url: pointQuery.data?.properties.observationStations,
-	// });
-
-	// const stationLatestObservationQuery = useGetStationLatestObservation({
-	// 	stationBaseUrl: stationsQuery.data?.observationStations?.[0],
-	// });
+	const pointQuery = useGetPoint({ lat, long });
 
 	return {
 		geolocation,
 		pointQuery,
-		// forecastQuery,
-		// hourlyForecastQuery,
-		// gridpointQuery,
-		// stationsQuery,
-		// stationLatestObservationQuery,
 	};
 };
