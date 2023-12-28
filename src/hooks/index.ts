5faa2566588d380c4f20b962bb3c2bacf136eecf
Eric Hicks <ehicks05@yahoo.com>
2023-12-28 16:09:12 -0500

    wire up active location switching

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,6 @@
 import { useWeatherGov } from './useWeatherGov';
 import { useOpenWeatherMap } from './useOpenWeatherMap';
+import { useSavedLocations } from './useSavedLocations';
+import { useActiveLocation } from './useActiveLocation';
 
-export { useWeatherGov, useOpenWeatherMap };
+export { useWeatherGov, useOpenWeatherMap, useActiveLocation, useSavedLocations };
