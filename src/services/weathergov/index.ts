bed580d3d560493ba83bfa1dc9d6017900201c94
Eric Hicks <ehicks05@yahoo.com>
2023-12-29 13:55:37 -0500

    cleanup weathergov api

--- src/services/weathergov/index.ts
+++ src/services/weathergov/index.ts
@@ -1,8 +1,2 @@
-export { useGetForecast } from './useGetForecast';
-export { useGetGridpoint } from './useGetGridpoint';
-export { useGetHourlyForecast } from './useGetHourlyForecast';
 export { useGetPoint } from './useGetPoint';
-export { useGetStationLatestObservation } from './useGetStationLatestObservation';
-export { useGetStations } from './useGetStations';
 export type { NOAACordinate } from './types';
-export type { Forecast } from './forecast_types';
