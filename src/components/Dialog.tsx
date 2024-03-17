736969a37263723e97c9f576e62bbe6dd50f5094
Eric Hicks <ehicks05@yahoo.com>
2024-03-17 10:50:22 -0400

    fix styling on light mode location modal

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode } from 'react';
 import { Dialog as HUIDialog } from '@headlessui/react';
+import React, { ReactNode } from 'react';
 
 interface DialogProps {
 	isOpen: boolean;
@@ -15,12 +15,12 @@ const Dialog = ({ isOpen, onClose, children }: DialogProps) => (
 	>
 		<HUIDialog.Overlay className="fixed inset-0 bg-black opacity-50" />
 		<div className="fixed inset-0 flex items-center justify-center p-4">
-			<HUIDialog.Panel className="bg-neutral-100 dark:bg-neutral-800 rounded-t-lg">
+			<HUIDialog.Panel className="bg-neutral-800 rounded-lg">
 				<div className="py-8 px-4 sm:px-6 lg:px-8 max-h-[90vh] overflow-auto">
 					{children}
 				</div>
 
-				<div className="bg-neutral-200 dark:bg-neutral-700 sm:flex sm:flex-row-reverse rounded-b-lg">
+				<div className="bg-neutral-700 sm:flex sm:flex-row-reverse rounded-b-lg">
 					<button type="button" className="px-4 sm:px-6 py-3" onClick={onClose}>
 						Close
 					</button>
