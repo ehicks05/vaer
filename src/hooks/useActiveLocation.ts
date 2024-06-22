53ec6cc3e81510e79fb17857c01f557c1f32be05
Eric Hicks <ehicks05@yahoo.com>
2024-06-22 11:26:27 -0400

    simplify useWeatherGov

--- src/hooks/useActiveLocation.ts
+++ src/hooks/useActiveLocation.ts
@@ -4,6 +4,10 @@ import { useLocalStorage } from '@uidotdev/usehooks';
 export type ActiveLocation = SearchResultGeoname | undefined;
 const key = 'vaer-active-location';
 
+/**
+ *
+ * @returns The Geoname result of the user's active location. If the user has selected 'Current Location', this will be undefined.
+ */
 export const useActiveLocation = () => {
 	return useLocalStorage<ActiveLocation>(key, undefined);
 };
--- src/hooks/useWeatherGov.ts
+++ src/hooks/useWeatherGov.ts
@@ -1,19 +1,10 @@
 import { useGetPoint } from '@/services/weathergov';
-import { useCachedGeolocation } from './useCachedGeolocation';
-import { useActiveLocation } from './useActiveLocation';
+import { useResolvedLocation } from './useResolvedLocation';
 
 export const useWeatherGov = () => {
-	const geolocation = useCachedGeolocation();
-	const [activeLocation] = useActiveLocation();
-
-	const { lat, long } = activeLocation
-		? { lat: activeLocation.lat, long: activeLocation.lng }
-		: { lat: String(geolocation.latitude), long: String(geolocation.longitude) };
+	const { lat, long } = useResolvedLocation();
 
 	const pointQuery = useGetPoint({ lat, long });
 
-	return {
-		geolocation,
-		pointQuery,
-	};
+	return { pointQuery };
 };
