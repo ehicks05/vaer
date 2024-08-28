12386f96d845da6a62b64491ec96b5c56ec0d569
Eric Hicks <ehicks05@yahoo.com>
2024-08-28 12:29:24 -0400

    update icon

---
+++ public/readme.md
@@ -0,0 +1,9 @@
+Notes on how to get icons:
+
+1. Using https://favicon.io/, you can either:
+   1. create a letter-based favicon, choosing colors, fonts, and sizes
+   2. convert an existing image
+
+Either way, you should end up with a handful of files.
+
+3. Use https://maskable.app/editor to resize one of the images to be maskable.
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,16 +3,15 @@ import { PreferredTempToggle } from '@/app/PreferredTemperature';
 
 const Logo = () => (
 	<div className="flex-shrink-0 flex items-center">
-		<div className="flex items-center gap-4">
+		<div className="flex items-center gap-1">
 			<img
 				className="inline h-8 w-auto"
-				src="/android-chrome-192x192.png"
+				src="/icon-gradient-transparent.svg"
 				alt="logo"
 			/>
 			<span className="mt-1 hidden sm:inline font-bold text-5xl font-logo text-white">
 				Vær
 			</span>
-			{/* <img className="hidden sm:inline h-8 w-auto" src="/text.svg" alt="logo" /> */}
 		</div>
 	</div>
 );
