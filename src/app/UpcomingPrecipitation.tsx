ab1cb1d776e34a499a160ebfe40ba53275205c21
Eric Hicks <ehicks05@yahoo.com>
2024-09-05 15:09:29 -0400

    honor units, more styling

--- src/app/UpcomingPrecipitation.tsx
+++ src/app/UpcomingPrecipitation.tsx
@@ -1,34 +1,43 @@
 import { Card } from '@/components';
 import { useOpenWeatherMap } from '@/hooks';
+import { useUnits } from '@/hooks/useUnits';
 import type { Minutely } from '@/services/openweathermap/types/oneCall';
-import { round } from 'lodash-es';
+import type { ReactNode } from 'react';
 
 const hmm = new Intl.DateTimeFormat('en-US', {
 	hour: 'numeric',
 	minute: '2-digit',
 });
 
-const Minute = ({ minute: { precipitation } }: { minute: Minutely }) => {
-	const color =
-		precipitation === 0
-			? 'bg-indigo-600'
-			: precipitation < 1
-				? 'bg-indigo-400'
-				: precipitation < 5
-					? 'bg-indigo-200'
-					: precipitation < 10
-						? 'bg-indigo-100'
-						: precipitation < 20
-							? 'bg-indigo-50'
-							: '';
+const INTENSITIES = [
+	{ color: 'bg-indigo-500', min: 0.0, max: 0.0 },
+	{ color: 'bg-indigo-400', min: 0.0, max: 0.1 },
+	{ color: 'bg-indigo-300', min: 0.1, max: 0.2 },
+	{ color: 'bg-indigo-200', min: 0.2, max: 0.4 },
+	{ color: 'bg-indigo-100', min: 0.4, max: 1.0 },
+	{ color: 'bg-indigo-50', min: 1.0, max: Number.POSITIVE_INFINITY },
+];
 
-	const style = { height: `${(precipitation || 0.01) * 32}px` };
-	return <div className={`rounded-sm w-4 ${color}`} style={style} />;
+const getIntensity = (inPerHour: number) =>
+	INTENSITIES.find(({ min, max }) => inPerHour >= min && inPerHour <= max) ||
+	INTENSITIES[0];
+
+interface Props {
+	max: number;
+	minute: Minutely;
+}
+
+const Minute = ({ max, minute: { precipitation } }: Props) => {
+	const intensity = getIntensity(precipitation);
+	const normalized = precipitation / max;
+	const style = { height: `${(normalized || 0.01) * 100}%` };
+	return <div className={`rounded-sm w-4 ${intensity.color}`} style={style} />;
 };
 
 export const UpcomingPrecipitation = () => {
+	const { getRate } = useUnits();
 	const { oneCallQuery } = useOpenWeatherMap();
-	const { data } = oneCallQuery;
+	const { data, dataUpdatedAt } = oneCallQuery;
 	if (!data) {
 		return null;
 	}
@@ -36,36 +45,43 @@ export const UpcomingPrecipitation = () => {
 
 	const min = Math.min(...minutely.map(({ precipitation }) => precipitation));
 	const max = Math.max(...minutely.map(({ precipitation }) => precipitation));
-	if (max === 0) {
-		return null;
-	}
-
 	const [start, mid, end] = [0, minutely.length / 2, minutely.length - 1].map(
 		(index) => hmm.format(new Date(minutely[index]?.dt || 0)),
 	);
 
+	if (max === 0) {
+		return <Container>No precipitation until at least {end}</Container>;
+	}
+
 	return (
-		<div className="flex flex-col">
-			Upcoming Precipitation [experimental]
-			<Card className="p-4 flex flex-col gap-1 h-36 justify-end">
-				<div className="flex flex-col gap-1 w-full">
-					<div className="flex items-end gap-0.5">
-						{minutely.map((minute) => (
-							<Minute key={minute.dt} minute={minute} />
-						))}
-					</div>
-					<div className="flex justify-between w-full text-sm">
-						<span>{start}</span>
-						<span>{mid}</span>
-						<span>{end}</span>
-					</div>
-				</div>
-				<div className="flex gap-2 w-full text-xs">
+		<Container>
+			<div className="h-36 flex flex-col gap-1">
+				<div className="flex justify-between gap-2 w-full text-xs">
+					<span>checked at {hmm.format(new Date(dataUpdatedAt))}</span>
 					<span>
-						range: {round(min, 2)} - {round(max, 2)} mm/h
+						range: {getRate(min)} - {getRate(max)}
 					</span>
 				</div>
-			</Card>
-		</div>
+				<div className="flex flex-col gap-1 w-full h-full">
+					<div className="flex items-end gap-[2px] h-full">
+						{minutely.map((minute) => (
+							<Minute key={minute.dt} max={max} minute={minute} />
+						))}
+					</div>
+				</div>
+				<div className="flex justify-between w-full text-sm">
+					<span>{start}</span>
+					<span>{mid}</span>
+					<span>{end}</span>
+				</div>
+			</div>
+		</Container>
 	);
 };
+
+const Container = ({ children }: { children: ReactNode }) => (
+	<div className="flex flex-col">
+		Upcoming Precipitation [experimental]
+		<Card className="p-4 flex flex-col gap-1 justify-end">{children}</Card>
+	</div>
+);
--- src/hooks/useUnits.ts
+++ src/hooks/useUnits.ts
@@ -14,35 +14,45 @@ export const useUnits = () => {
 	const { unitSystem } = useUnitSystem();
 	const isMetric = unitSystem === 'metric';
 
-	return {
-		getLength: (inches: number) => {
-			const inPreferredUnit = isMetric ? inchToMm(inches) : inches;
-			const precision = isMetric ? 1 : 2;
-			const rounded = round(inPreferredUnit, precision);
-			// logic: both zeroes and truthy rounded values are fine,
-			// but if our rounding turns a non-zero into zero, show a '<' sign.
-			const display = rounded
-				? rounded
-				: !inches
-					? 0
-					: `< .${'0'.repeat(precision - 1)}1`;
+	const getLength = (inches: number) => {
+		const inPreferredUnit = isMetric ? inchToMm(inches) : inches;
+		const precision = isMetric ? 1 : 2;
+		const rounded = round(inPreferredUnit, precision);
+		// logic: both zeroes and truthy rounded values are fine,
+		// but if our rounding turns a non-zero into zero, show a '<' sign.
+		const display = rounded
+			? rounded
+			: !inches
+				? 0
+				: `< .${'0'.repeat(precision - 1)}1`;
 
-			const unit = isMetric ? 'mm' : 'in';
-			return `${display} ${unit}`;
-		},
-		getSpeed: (mph: number) => {
-			const inPreferredUnit = isMetric ? mphToKmh(mph) : mph;
-			const unit = isMetric ? 'kmh' : 'mph';
-			return `${Math.round(inPreferredUnit)} ${unit}`;
-		},
-		getTemp: (f: number) => {
-			const inPreferredUnit = isMetric ? fToC(f) : f;
-			return `${Math.round(inPreferredUnit)}\u00B0`;
-		},
-		getPressure: (hpa: number) => {
-			const inPreferredUnit = isMetric ? hpa : hPaToInHg(hpa);
-			const unit = isMetric ? 'hPa' : 'inHg';
-			return `${round(inPreferredUnit, 2)} ${unit}`;
-		},
+		const unit = isMetric ? 'mm' : 'in';
+		return `${display} ${unit}`;
+	};
+
+	const getRate = (inPerHour: number) => {
+		return `${getLength(inPerHour)}/h`;
+	};
+	const getSpeed = (mph: number) => {
+		const inPreferredUnit = isMetric ? mphToKmh(mph) : mph;
+		const unit = isMetric ? 'kmh' : 'mph';
+		return `${Math.round(inPreferredUnit)} ${unit}`;
+	};
+	const getTemp = (f: number) => {
+		const inPreferredUnit = isMetric ? fToC(f) : f;
+		return `${Math.round(inPreferredUnit)}\u00B0`;
+	};
+	const getPressure = (hpa: number) => {
+		const inPreferredUnit = isMetric ? hpa : hPaToInHg(hpa);
+		const unit = isMetric ? 'hPa' : 'inHg';
+		return `${round(inPreferredUnit, 2)} ${unit}`;
+	};
+
+	return {
+		getLength,
+		getRate,
+		getSpeed,
+		getTemp,
+		getPressure,
 	};
 };
