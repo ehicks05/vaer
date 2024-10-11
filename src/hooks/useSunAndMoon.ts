67fb27e9ffeca3b2316148e2b99f05bea17996c5
Eric Hicks <ehicks05@yahoo.com>
2024-10-11 14:26:03 -0400

    cleanup

--- src/hooks/useSunAndMoon.ts
+++ src/hooks/useSunAndMoon.ts
@@ -2,21 +2,15 @@ import { getMoonPhaseIcon } from '@/constants/weather_icons';
 import { getMoonIllumination, getMoonTimes, getTimes } from 'suncalc';
 import { useResolvedLocation } from './useResolvedLocation';
 
-export const useSunAndMoon = (date: Date) => {
-	const { lat, long } = useResolvedLocation();
-	const { sunrise, sunset } = getTimes(
-		date ? new Date(date) : new Date(),
-		Number(lat),
-		Number(long),
-	);
-	const { rise: moonrise, set: moonset } = getMoonTimes(
-		date ? new Date(date) : new Date(),
-		Number(lat),
-		Number(long),
-	);
-	const { phase: moonPhase } = getMoonIllumination(
-		date ? new Date(date) : new Date(),
-	);
+export const useSunAndMoon = (_date?: Date) => {
+	const date = _date ? new Date(_date) : new Date();
+	const resolvedLocation = useResolvedLocation();
+	const lat = Number(resolvedLocation.lat);
+	const long = Number(resolvedLocation.long);
+
+	const { sunrise, sunset } = getTimes(date, lat, long);
+	const { rise: moonrise, set: moonset } = getMoonTimes(date, lat, long);
+	const { phase: moonPhase } = getMoonIllumination(date);
 	const { Icon: MoonPhaseIcon, label: moonPhaseLabel } = getMoonPhaseIcon(moonPhase);
 
 	return {
