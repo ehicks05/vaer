1bed141feb3fb49ffdd0fae8314f0d95ec23ea07
Eric Hicks <ehicks05@yahoo.com>
2024-10-16 16:20:03 -0400

    move

--- src/services/openweathermap/index.ts
+++ src/services/openweathermap/index.ts
@@ -1,6 +1,7 @@
+import { getWeatherIcon } from './icons';
 import { useAirPollution } from './useAirPollution';
 import { useFiveDay } from './useFiveDay';
 import { useOneCall } from './useOneCall';
 import { useReverseGeo } from './useReverseGeo';
 
-export { useOneCall, useAirPollution, useFiveDay, useReverseGeo };
+export { useOneCall, useAirPollution, useFiveDay, useReverseGeo, getWeatherIcon };
