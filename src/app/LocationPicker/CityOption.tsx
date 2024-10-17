6a3038fe6bbf697d5db7fe235220a5669a2ced76
Eric Hicks <ehicks05@yahoo.com>
2024-10-17 11:26:06 -0400

    make entire location 'mini card' clickable

--- src/app/LocationPicker/CityOption.tsx
+++ src/app/LocationPicker/CityOption.tsx
@@ -1,9 +1,5 @@
 import { type Geoname, geonameToLabel } from '@/services/geonames';
-import {
-	HiOutlineCheckCircle,
-	HiOutlinePlusCircle,
-	HiOutlineXCircle,
-} from 'react-icons/hi2';
+import { HiOutlineXCircle } from 'react-icons/hi2';
 
 interface Props {
 	city: Partial<Geoname>;
@@ -22,33 +18,22 @@ export const CityOption = ({
 	onActivate,
 	onDelete,
 }: Props) => {
+	const classes = isActive ? 'bg-green-800' : 'bg-neutral-700 hover:bg-neutral-600';
 	return (
-		<div className="w-full flex justify-between items-center gap-4 md:gap-8 p-2 text-sm md:text-base bg-neutral-700 rounded-lg">
-			<div className="flex items-center gap-4">
-				{isSaved && (
-					<HiOutlineCheckCircle
-						size={28}
-						onClick={onActivate}
-						className={
-							isActive
-								? 'flex-shrink-0 cursor-pointer text-green-500 hover:text-green-400'
-								: 'flex-shrink-0 cursor-pointer text-neutral-500 hover:text-neutral-400'
-						}
-					/>
-				)}
-				<div className="text-sm">{geonameToLabel(city)}</div>
-			</div>
+		<button
+			type="button"
+			disabled={isActive}
+			onClick={isSaved ? onActivate : onClick}
+			className={`w-full flex justify-between items-center gap-4 md:gap-8 p-2 text-sm rounded-lg ${classes}`}
+		>
+			<div className="text-left">{geonameToLabel(city)}</div>
 			<div className="flex items-center gap-2">
-				{!isSaved && (
-					<HiOutlinePlusCircle
-						onClick={onClick}
-						size={28}
-						className="flex-shrink-0 cursor-pointer text-green-500 hover:text-green-400"
-					/>
-				)}
 				{isSaved && (
 					<HiOutlineXCircle
-						onClick={onDelete}
+						onClick={(e) => {
+							e.stopPropagation();
+							onDelete ? onDelete() : undefined;
+						}}
 						size={28}
 						className={
 							onDelete
@@ -58,6 +43,6 @@ export const CityOption = ({
 					/>
 				)}
 			</div>
-		</div>
+		</button>
 	);
 };
