1bd161f75d8da35d1a66f0dbeb1920417a4dc86a
Eric Hicks <ehicks05@yahoo.com>
2024-10-18 10:26:16 -0400

    cleanup

--- src/constants/fmt.ts
+++ src/constants/fmt.ts
@@ -1,17 +1,4 @@
-export const meterFmt = Intl.NumberFormat('en-US', {
-	unit: 'meter',
-	notation: 'compact',
-	maximumSignificantDigits: 2,
-});
-
-export const weekdayShort = Intl.DateTimeFormat('en-US', { weekday: 'short' });
-export const weekdayLong = Intl.DateTimeFormat('en-US', { weekday: 'long' });
-
-export const hour = Intl.DateTimeFormat('en-US', { hour: 'numeric' });
-
 export const alertFmt = Intl.DateTimeFormat('en-US', {
 	dateStyle: 'medium',
 	timeStyle: 'short',
 });
-export const timeShort = Intl.DateTimeFormat('en-US', { timeStyle: 'short' });
-export const dateShort = Intl.DateTimeFormat('en-US', { dateStyle: 'short' });
