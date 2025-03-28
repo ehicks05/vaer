cb588b220b7640c2388d6137fd3d840325997044
Eric Hicks <ehicks05@yahoo.com>
2025-03-28 16:34:36 -0400

    index

--- src/services/openweathermap/index.ts
+++ src/services/openweathermap/index.ts
@@ -1,7 +1,31 @@
-import { getWeatherIcon } from './icons';
+import { useResolvedLatLong } from '../../hooks/useResolvedLatLong';
 import { useAirPollution } from './useAirPollution';
 import { useFiveDay } from './useFiveDay';
 import { useOneCall } from './useOneCall';
-import { useReverseGeo } from './useReverseGeo';
 
-export { useOneCall, useAirPollution, useFiveDay, useReverseGeo, getWeatherIcon };
+/**
+ *
+ * @returns Various OWM queries, with lat and long set to activeLocation if present,
+ * otherwise current geolocation.
+ */
+export const useOpenWeatherMap = () => {
+	const { lat, long } = useResolvedLatLong();
+
+	const oneCallQuery = useOneCall({ lat, long });
+	const airPollutionQuery = useAirPollution({ lat, long });
+
+	return { oneCallQuery, airPollutionQuery };
+};
+
+/**
+ *
+ * @returns Separate hook for a forecast query that won't
+ * run on page-load
+ */
+export const useOpenWeatherMapFiveDay = () => {
+	const { lat, long } = useResolvedLatLong();
+
+	const fiveDayQuery = useFiveDay({ lat, long });
+
+	return { fiveDayQuery };
+};
