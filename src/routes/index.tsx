54399ffc50991c7222f81b474cae7c0eb668976b
Eric Hicks <ehicks05@yahoo.com>
2025-03-28 16:46:12 -0400

    queryclient, providers

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,37 @@
-import { createFileRoute, useRouter } from '@tanstack/react-router';
+import { Footer, Header } from '@/components';
+import { AppProvider } from '@/contexts/AppProvider';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createFileRoute } from '@tanstack/react-router';
+import { Home } from '../app/Home';
 
 export const Route = createFileRoute('/')({
-	component: Home,
-	loader: async () => ({ im: 'cool' }),
+	component: Index,
+	ssr: false,
 });
 
-function Home() {
-	const router = useRouter();
-	const state = Route.useLoaderData();
+const queryClient = new QueryClient();
 
-	return <div>{state.im}</div>;
+const persister = createSyncStoragePersister({
+	storage: window.localStorage,
+	key: 'vaer-REACT_QUERY_OFFLINE_CACHE',
+});
+
+function Index() {
+	return (
+		<PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+			<AppProvider>
+				<div className="flex flex-col min-h-screen bg-gradient-to-r from-stone-900 to-neutral-950">
+					<div className="sm:px-4">
+						<Header />
+					</div>
+					<div className="flex-grow flex flex-col h-full sm:px-4">
+						<Home />
+					</div>
+					<Footer />
+				</div>
+			</AppProvider>
+		</PersistQueryClientProvider>
+	);
 }
