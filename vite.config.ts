3e129055cd3ae360fbe33a065dc4beaa8595c74b
Eric Hicks <ehicks05@yahoo.com>
2025-07-31 17:16:21 -0400

    tw

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	server: {
@@ -9,8 +10,9 @@ export default defineConfig({
 		host: '0.0.0.0',
 	},
 	plugins: [
-		tsConfigPaths(),
 		tanstackStart({ customViteReactPlugin: true, target: 'vercel' }),
+		tsConfigPaths(),
+		tailwindcss(),
 		viteReact(),
 	],
 });
