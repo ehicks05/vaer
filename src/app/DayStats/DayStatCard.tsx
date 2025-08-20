d7c93fa78003019690c2bfa2c4662e19be520d4f
Eric Hicks <ehicks05@yahoo.com>
2025-08-20 13:23:33 -0400

    fit

--- src/app/DayStats/DayStatCard.tsx
+++ src/app/DayStats/DayStatCard.tsx
@@ -8,11 +8,11 @@ interface DayStat {
 }
 
 export const DayStatCard = ({ stat: { Icon, label, value } }: { stat: DayStat }) => (
-	<Card className="flex items-center gap-2 p-4">
+	<Card className="flex items-center gap-1 lg:gap-2 p-2 lg:p-4">
 		<Icon size={32} className="shrink-0" />
 		<div>
 			<div className="text-xs text-neutral-400">{label}</div>
-			<div>{value}</div>
+			<div className='text-sm lg:text-base'>{value}</div>
 		</div>
 	</Card>
 );
