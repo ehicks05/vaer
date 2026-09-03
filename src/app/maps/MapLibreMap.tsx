b0385769808b10a9437bea8b7f3ecd700b36fabd
Eric Hicks <ehicks05@yahoo.com>
2026-09-03 15:43:36 -0400

    refetch

--- src/app/maps/MapLibreMap.tsx
+++ src/app/maps/MapLibreMap.tsx
@@ -3,28 +3,38 @@ import { addProtocol, setWorkerUrl } from 'maplibre-gl';
 import MapLibre, { Layer, Source } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
+import { useState } from 'react';
+import { useInterval } from 'usehooks-ts';
 
 setWorkerUrl(workerUrl);
 
 addProtocol('om', omProtocol);
-const omUrl =
-	'https://openmeteo.s3.amazonaws.com/data_spatial/dwd_icon/latest.json?time_step=current_time_1H&variable=precipitation&dark=true';
 
 const MAP_TILER_API_KEY = 'Z0saQSNyZmNKMPbZnckK';
 const mapStyle = `https://api.maptiler.com/maps/dataviz-v4-dark/style.json?key=${MAP_TILER_API_KEY}`;
+
+const OM_BASE =
+	'https://openmeteo.s3.amazonaws.com/data_spatial/dwd_icon/latest.json';
+const OM_DEFAULTS = {
+	time_step: 'current_time_1M',
+	variable: 'precipitation',
+	dark: 'true',
+};
 
 interface Props {
 	coords: [number, number];
 }
 
 export function MapLibreMap({ coords: [latitude, longitude] }: Props) {
+	const [t, setT] = useState(Date.now().toString());
+	useInterval(() => setT(Date.now().toString()), 1000 * 60);
+
+	const omParams = new URLSearchParams({ ...OM_DEFAULTS, t }).toString();
+	const omUrl = `${OM_BASE}?${omParams}`;
+
 	return (
 		<MapLibre
-			initialViewState={{
-				longitude,
-				latitude,
-				zoom: 6,
-			}}
+			initialViewState={{ longitude, latitude, zoom: 6 }}
 			style={{ width: '100%', height: '100%', borderRadius: '8px' }}
 			mapStyle={mapStyle}
 			attributionControl={false}
