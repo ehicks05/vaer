4861594b4ced4f84613ab527290b84f0b11ad99f
Eric Hicks <ehicks05@yahoo.com>
2026-09-04 16:26:30 -0400

    simplify

--- src/features/HourlyForecast/ScrollbarContainer.tsx
+++ src/features/HourlyForecast/ScrollbarContainer.tsx
@@ -1,12 +1,9 @@
 import type { ReactNode } from 'react';
 import { Card } from '@/components';
 
-export const SCROLLBAR_CLASS =
-	'overflow-x-scroll scrollbar-thin scrollbar-track-transparent scrollbar-thumb-transparent group-hover:scrollbar-thumb-slate-800 scrollbar-track-rounded-lg scrollbar-thumb-rounded-lg';
-
 export const ScrollbarContainer = ({ children }: { children: ReactNode }) => (
 	<Card>
-		<div className={`flex gap-6 overflow-auto p-4 pb-2 ${SCROLLBAR_CLASS}`}>
+		<div className="flex gap-6 p-4 overflow-x-scroll scroll-fade-TODO">
 			{children}
 		</div>
 	</Card>
