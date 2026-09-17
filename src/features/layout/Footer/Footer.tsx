651c51fe651ed34e1fc4956a0a7aa5e718626141
Eric Hicks <ehicks05@yahoo.com>
2026-09-17 11:59:43 -0400

    Update Footer.tsx

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -1,7 +1,13 @@
 import { Debug } from './Debug';
 
-export const Footer = () => (
-	<footer className="flex items-center justify-end gap-4 p-4 max-w-7xl w-full mx-auto">
-		<Debug />
-	</footer>
-);
+export const Footer = () => {
+	if (import.meta.env.PROD) {
+		return null;
+	}
+
+	return (
+		<footer className="flex items-center justify-end gap-4 p-4 max-w-7xl w-full mx-auto">
+			<Debug />
+		</footer>
+	);
+};
